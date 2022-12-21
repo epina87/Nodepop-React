b7f36fe9415a4c2eb186f0cb5dde4fd235f638a1
Jordi <jordi@iceverg.com>
2022-12-21 19:28:08 +0100

    Usamos una función para randomizar elementos de un array

--- index.js
+++ index.js
@@ -33,7 +33,30 @@ for(let i = 0; i < ROWS; i++) {
     }
     board[i] = row
 }
-// console.log(board)
+
+
+// una funcion que nos permite randomizar los elementos de un array
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
+board = shuffle(board)
+console.log(board)
+
 console.log('Initial table')
 for(let i = 0; i < ROWS; i++) {
     console.log(board[i])
