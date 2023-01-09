fdf89385f5bced3025e72d467dfa095c73ecf9e8
Jordi <jordi@iceverg.com>
2023-01-09 20:16:56 +0100

    Más ejemplos de clases con sintaxis tradicional

--- demos/callbacks.js
+++ demos/callbacks.js
@@ -1,15 +1,32 @@
-class Personaje {
-    constructor(name, attackSpeed = 0, attackDamage = 0){
-        this.name = name
-        this.attackSpeed = attackSpeed
-        this.attackDamage = attackDamage
-        console.log(`Construyendo ${name}`)
-    }
+// class Personaje {
+//     constructor(name, attackSpeed = 0, attackDamage = 0){
+//         this.name = name
+//         this.attackSpeed = attackSpeed
+//         this.attackDamage = attackDamage
+//         console.log(`Construyendo ${name}`)
+//     }
+// }
+
+function Personaje(name, attackSpeed = 0, attackDamage = 0) {
+    this.name = name
+    this.attackSpeed = attackSpeed
+    this.attackDamage = attackDamage
+    console.log(`Construyendo ${name}`)
 }
 
-const legolas = new Personaje('legolas',20,10)
-const aragorn = new Personaje('aragorn',15,15)
-const gimly = new Personaje('gimly',7,20)
+// objetos de clase Personaje
+const legolas = new Personaje('legolas', 20, 10)
+const aragorn = new Personaje('aragorn', 15, 15)
+const gimly = new Personaje('gimly', 7, 20)
+
+console.log(legolas instanceof Personaje)
+console.log(legolas instanceof Object)
+
+// Objeto literal
+let saruman = { name: 'saruman' }
+saruman.__proto__ = new Personaje()
+console.log(saruman instanceof Personaje)
+console.log(saruman instanceof Object)
 
 function atacar(personaje, callback) { //   si callback === attackWithSword
     let attack = callback(personaje) //     let attack = attackWithSword(personaje) 
@@ -43,4 +60,4 @@ function attackWithAxe(person) {
 atacar(gimly, attackWithAxe)
 atacar(aragorn, attackWithAxe)
 atacar(aragorn, attackWithSword)
-atacar({name: 'saruman'}, attackWithBowAndArrow) // saruman ataca con ataque Bow & Arrow causando NaN a velocidad NaN
+atacar(saruman, attackWithBowAndArrow) // saruman ataca con ataque Bow & Arrow causando NaN a velocidad NaN
